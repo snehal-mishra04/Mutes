06cb25a3aadd65886adb82d0dfabf00b41295943
Snehal Mishra <snehalmishra0404@gmail.com>
2026-09-03 11:09:07 +0530

    Fix React Router base path for GitHub Pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -15,117 +15,117 @@ import CouponPage from "./pages/UserDashboard/CouponPage.jsx";
 import SupportPage from "./pages/UserDashboard/SupportPage.jsx";
 import AddressPage from "./pages/UserDashboard/Address/AddressPage.jsx";
 import CollectionPage from "./pages/UserDashboard/CollectionPage/CollectionPage.jsx";
-import ProductDetails  from "./pages/ProductDetails/ProductDetails.jsx";
+import ProductDetails from "./pages/ProductDetails/ProductDetails.jsx";
 import OtpForm from "./pages/AuthPage/OtpForm.jsx";
 import { Provider } from "react-redux";
 import { store } from "./Redux/store/store.jsx";
 
-import  RequireAuth from "./utils/RequireAuth.jsx";
+import RequireAuth from "./utils/RequireAuth.jsx";
 import { Toaster } from "react-hot-toast";
-import Payment from "././pages/payment/Payment.jsx";
+import Payment from "./pages/payment/Payment.jsx";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+
+      children: [
+        {
+          path: "/collections/gender/:gender",
+          element: <CollectionPage />,
+        },
+        {
+          path: "",
+          element: <Home />,
+        },
+        {
+          path: "/collections/category/:category",
+          element: <CollectionPage />,
+        },
+        {
+          path: "/collections/condition/:condition",
+          element: <CollectionPage />,
+        },
+        {
+          path: "/product-details/:id",
+          element: <ProductDetails />,
+        },
+        {
+          path: "/cart",
+          element: <Cart />,
+        },
+        {
+          path: "/Wishlist",
+          element: <WishlistPage />,
+        },
+        {
+          path: "/orders",
+          element: <OrderPage />,
+        },
+        {
+          path: "/gift-cards",
+          element: <GiftCardPage />,
+        },
+        {
+          path: "/coupons",
+          element: <CouponPage />,
+        },
+        {
+          path: "/help",
+          element: <SupportPage />,
+        },
+        {
+          path: "/collection",
+          element: <CollectionPage />,
+        },
+        {
+          path: "/auth/login",
+          element: <Login />,
+        },
+        {
+          path: "/auth/sign-up",
+          element: <SignUp />,
+        },
+        {
+          path: "/auth/verify-otp",
+          element: <OtpForm />,
+        },
+      ],
+    },
+
+    {
+      path: "/checkout",
+      children: [
+        {
+          path: "address",
+          element: (
+            <RequireAuth>
+              <AddressPage />
+            </RequireAuth>
+          ),
+        },
+        {
+          path: "payment",
+          element: (
+            <RequireAuth>
+              <Payment />
+            </RequireAuth>
+          ),
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-
-
-    children: [
-      {
-        path: "/collections/gender/:gender",
-        element: <CollectionPage/>,
-      },
-      {
-        path: "",
-        element: <Home />,
-      },
-
-      {
-        path: "/collections/category/:category",
-        element: <CollectionPage/>,
-      },
-      {
-        path: "/collections/condition/:condition",
-        element: <CollectionPage/>,
-      },
-
-      {
-        path: "/product-details/:id",
-        element: <ProductDetails />,
-      },
-      {
-        path: "/cart",
-        element: <Cart />,
-      },
-      {
-        path: "/Wishlist",
-        element: <WishlistPage />,
-      },
-      {
-        path: "/orders",
-        element: <OrderPage />,
-      },
-      {
-        path: "/gift-cards",
-        element: <GiftCardPage />,
-      },
-      {
-        path: "/coupons",
-        element: <CouponPage />,
-      },
-      {
-        path: "/help",
-        element: <SupportPage />,
-      },
-      {
-        path: "/collection",
-        element: <CollectionPage />,
-      },
-      {
-        path: "/auth/login",
-        element: <Login />,
-      },
-      {
-        path: "/auth/sign-up",
-        element: <SignUp />,
-      },
-      {
-        path: "/auth/verify-otp",
-        element: <OtpForm />,
-      },
-    ],
-  },
-  {
-    path:'/checkout',
-    children:[
-      {
-        path:'address',
-        element:( 
-          <RequireAuth>
-            <AddressPage/>
-          </RequireAuth>
-        )
-      },
-
-      {
-        path:'payment',
-        element:( 
-          <RequireAuth>
-            <Payment/>
-          </RequireAuth>
-        )
-      }
-    ]
-  },
-
- 
-]);
+    basename: "/Mutes",
+  }
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-            <Toaster
-        position="top-right" 
+      <Toaster
+        position="top-right"
         toastOptions={{
           duration: 5000,
           style: {
@@ -135,7 +135,7 @@ createRoot(document.getElementById("root")).render(
             borderRadius: "10px",
             fontSize: "14px",
             boxShadow: "0 8px 20px rgba(0,0,0,0.4)",
-            height:"70px"
+            height: "70px",
           },
           success: {
             style: {
@@ -146,11 +146,12 @@ createRoot(document.getElementById("root")).render(
             style: {
               background: "#FFFFFF",
               color: "#000000",
-              border: "1px solid #FFFFFF"
+              border: "1px solid #FFFFFF",
             },
           },
         }}
       />
+
       <RouterProvider router={router} />
     </Provider>
   </StrictMode>
